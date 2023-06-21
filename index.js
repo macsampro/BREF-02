19530a1da55fb353d2ba2c946da7abebd56dcab0
macsampro <112323681+macsampro@users.noreply.github.com>
2023-06-21 12:12:48 +0200

    integration

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
-document.getElementById('myForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Empêche le rechargement de la page
-    let prenom = document.getElementById('fname').value;
-    console.log('Prénom :', prenom);
-  });
+
+// ecoute du boutton de validation:
+document.addEventListener('DOMContentLoaded', function() {
+  let button = document.getElementById("buttoninput");
+  button.addEventListener("click", function(){
+    validationEmail();
+    validationPass();
+    console.log(button);
+  })
+  }); 
   
+  //fonction verification mail:
+  function validationMail(params) {
+    let mailinput = document.getElementById(mailinput); 
+    let emailError = document.getElementById(emailError);
+    let email =  mailinput.value;
+    let emailRegex = /^[^\s@]+@[^\s@]+.[^\s@]+$/; // FORMAT EMAIL !!! NE VERIFIE QUE LA SYNTAXE
+
+
+    if (email === "") {
+      mailinput.style.borderColor= "red";
+      emailError.style.color = "red";
+      emailError. innerText = "L'adresse e-mail n'est pas valide";
+    }else if(!emailRegex.test(email)){
+      mailinput.style.borderColor= "red";
+      emailError.style.color = "red";
+      emailError. innerText = "L'adresse e-mail n'est pas valide";
+
+    }else{
+      mailinput.style.borderColor= "";
+      emailError. innerText = "";
+
+    }
+  }
+
+
+
