3b65863d981d32fa8c9ed01beef20ce0971f9ea9
kent108 <quentin.cutrim@gmail.com>
2023-06-21 14:09:32 +0200

    changement js 14:10

--- index.js
+++ index.js
@@ -1,37 +1,38 @@
 
 // ecoute du boutton de validation:
-document.addEventListener('DOMContentLoaded', function() {
+// document.addEventListener('DOMContentLoaded', function() {
   let button = document.getElementById("buttoninput");
   button.addEventListener("click", function(){
-    validationEmail();
-    validationPass();
+    validationMail();
+    // validationPass();
     console.log(button);
   })
-  }); 
+  // }); 
   
   //fonction verification mail:
-  function validationMail(params) {
-    let mailinput = document.getElementById(mailinput); 
-    let emailError = document.getElementById(emailError);
+  function validationMail() {
+    let mailinput = document.getElementById("inputmail"); 
+    let mailerror = document.getElementById("erroremail");
     let email =  mailinput.value;
     let emailRegex = /^[^\s@]+@[^\s@]+.[^\s@]+$/; // FORMAT EMAIL !!! NE VERIFIE QUE LA SYNTAXE
 
 
     if (email === "") {
-      mailinput.style.borderColor= "red";
-      emailError.style.color = "red";
-      emailError. innerText = "L'adresse e-mail n'est pas valide";
+      erroremail.innerText = "L'adresse e-mail n'est pas valide";
+      inputmail.style.borderColor = "red";
+      erroremail.style.color = "red";
+    
+      
     }else if(!emailRegex.test(email)){
-      mailinput.style.borderColor= "red";
-      emailError.style.color = "red";
-      emailError. innerText = "L'adresse e-mail n'est pas valide";
+      inputmail.style.borderColor = "red";
+      erroremail.style.color = "red";
+      erroremail. innerText = "L'adresse e-mail n'est pas valide";
 
     }else{
-      mailinput.style.borderColor= "";
-      emailError. innerText = "";
+      erroremail.style.borderColor= "";
+      erroremail. innerText = "";
 
     }
-  }
-
-
+};
+  
 
